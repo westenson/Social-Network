8681be4fdfbbedd5fbd1cdcfe14b93ffc66b3ad7
drgerstl <56102052+drgerstl@users.noreply.github.com>
2019-12-02 20:03:20 -0600

    Removed temp code. Organized my methods. Added header comment

--- aTeam-CS400/application/Main.java
+++ aTeam-CS400/application/Main.java
@@ -1,7 +1,6 @@
 package application;
 	
 import java.util.ArrayList;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -17,10 +16,29 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+
+/***************************************************************************************************
+*
+* @author 		Dan Gerstl
+*
+* @version	 	1.0 <p>
+*
+* File:			Main.java<p>
+*
+* Date:			December 2, 2019<p>
+*
+* Purpose:		aTeam p2 - GUI
+*
+* Description:  Creates a GUI as a mockup for our aTeam design and fills it with hardcoded data
+* 				to provide an example for what it will look like when the data structure is 
+* 				implemented.
+*
+* Comment:		
+*
+***************************************************************************************************/
 
 public class Main extends Application {
 	
@@ -30,28 +48,28 @@ public class Main extends Application {
 			
 			/*** Local Variables ***/
 			
-			BorderPane root = new BorderPane();
+			BorderPane root  = new BorderPane();
 			
-			VBox centerBox = new VBox();
+			VBox centerBox   = new VBox();
 			
-			HBox topPanel = new HBox();  
-			HBox bottomPanel = new HBox();  // TEMPORARY FOR SPACING PURPOSES			
+			HBox topPanel    = new HBox();  
+			HBox bottomPanel = new HBox(); 			
 			
 			/*** Add components to centerBox ***/
 			
 			centerBox.getChildren().add(createCanvasPane());
 			centerBox.getChildren().add(createButtonPane());
 			
-			/*** Create Spacers ***/
+			/*** Create top and bottom panels ***/
 			
-			topPanel = createTopPanel();  // TEMPORARY FOR SPACING PURPOSES
-			bottomPanel = createBottomPanel();  // TEMPORARY FOR SPACING PURPOSES
+			topPanel = createTopPanel();  
+			bottomPanel = createBottomPanel();  
 			
 			/*** Add components to root ***/
 			
 			root.setTop(topPanel);		 
 			root.setCenter(centerBox);	
-			root.setBottom(bottomPanel);     // TEMPORARY FOR SPACING PURPOSES
+			root.setBottom(bottomPanel);     
 			
 			/*** Set scene ***/
 			
@@ -68,6 +86,8 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	
+	/*** Panel creation methods ***/
 	
 	private HBox createCanvasPane() {
 		
@@ -118,6 +138,125 @@ public class Main extends Application {
 				
 		return mainBox;
 	}
+		
+	private HBox createButtonPane() {
+		
+		/*** Local Constants ***/
+		
+		final double BUTTON_HEIGHT  = 20.0;
+		final double BUTTON_WIDTH   = 90.0;
+		final double BUTTON_SPACING = 10.0;
+		
+		/*** Local Variables ***/
+
+		HBox buttonPane = new HBox();
+		
+		ArrayList<Button> buttonList = new ArrayList<Button>();
+		
+		Button btnClear   = new Button("Clear"   );
+		Button btnNewUser = new Button("New User");
+		Button btnUndo    = new Button("Undo"    );
+		Button btnRedo    = new Button("Redo"    );
+		Button btnLoad    = new Button("Load"    );
+		Button btnExport  = new Button("Export"  );
+		Button btnExit    = new Button("Exit"    );
+		
+		/*** Add buttons to Array ***/
+		
+		buttonList.add(btnClear  );
+		buttonList.add(btnNewUser);
+		buttonList.add(btnUndo   );
+		buttonList.add(btnRedo   );
+		buttonList.add(btnLoad   );
+		buttonList.add(btnExport );
+		buttonList.add(btnExit   );	
+		
+		/*** Set spacing for buttons ***/
+		
+		buttonPane.setPadding(new Insets(0, 15, 0, 15));
+		buttonPane.setSpacing(BUTTON_SPACING);
+		
+		/*** Set button size and add to pane***/
+		
+		for (Button b : buttonList) {
+			
+			b.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+			
+			buttonPane.getChildren().add(b);
+		}
+		
+		return buttonPane;
+	}
+	
+	private HBox createTopPanel() { 
+		
+		HBox topPanel = new HBox();
+		
+		// create radio buttons for left-most feature
+    	RadioButton rb1 = new RadioButton("Show all friends");
+    	RadioButton rb2 = new RadioButton("Show mutual friends");
+    	RadioButton rb3 = new RadioButton("Show shortest path");
+
+        VBox tGroupContainer = new VBox(rb1, rb2, rb3);
+    	final ToggleGroup tGroup = new ToggleGroup();
+    	
+    	rb1.setSelected(true);
+    	rb1.setToggleGroup(tGroup);
+    	rb2.setToggleGroup(tGroup);
+    	rb3.setToggleGroup(tGroup);
+    	
+    	// create combo box for main profile
+    	VBox v1 = new VBox();
+    	Label l1 = new Label("Main Profile");
+    	ObservableList<String> users = 
+    			FXCollections.observableArrayList("User 1","User 2","User 3");
+    	ComboBox c1 = new ComboBox(users);
+    	v1.getChildren().addAll(l1, c1);
+    	
+    	// create combo box for friend
+    	VBox v2 = new VBox();
+    	Label l2 = new Label("Main Profile");
+    	ObservableList<String> friends = 
+    			FXCollections.observableArrayList("Friend 1","Friend 2","Friend 3");
+    	ComboBox c2 = new ComboBox(users);
+    	v2.getChildren().addAll(l2, c2);
+    	
+    	// create button for remove user and remove friendship
+		Button btnRmUser = new Button("Remove User");
+    	Button btnRmFriend = new Button("Remove Friendship");
+		
+		final double BUTTON_HEIGHT  = 20.0;
+		final double BUTTON_WIDTH   = 90.0;
+		
+		btnRmUser.setPrefSize(150.0, 20.0);
+		btnRmFriend.setPrefSize(150.0, 20.0);
+    	
+    	// add all nodes to top panel
+    	topPanel.getChildren().addAll(tGroupContainer, v1, v2, btnRmUser, btnRmFriend);
+		topPanel.setSpacing(20);
+
+		return topPanel;
+		
+	}
+	
+	private HBox createBottomPanel() { 
+		
+		HBox bottomPanel = new HBox();
+				
+		String prevAction = "Last Action Taken";
+		Label lastAction = new Label("Last Action Taken");
+		
+		int currentUsers = 0;
+		Label userCount = new Label("Total Current Users: " + currentUsers);
+		
+		bottomPanel.getChildren().addAll(lastAction, userCount);
+		bottomPanel.setSpacing(475);
+		
+		return bottomPanel;
+		
+	}
+	
+	/*** Canvas friend drawing method ***/
 	
 	private void drawExampleFriends(GraphicsContext gc, double x, double y) {
 		
@@ -190,137 +329,7 @@ public class Main extends Application {
 		gc.strokeLine(CIRCLE_WIDTH, CIRCLE_HEIGHT / 2, FRIEND_X_OFFSET, CIRCLE_HEIGHT / 2);		
 	}
 	
-	private HBox createButtonPane() {
-		
-		/*** Local Constants ***/
-		
-		final double BUTTON_HEIGHT  = 20.0;
-		final double BUTTON_WIDTH   = 90.0;
-		final double BUTTON_SPACING = 10.0;
-		
-		/*** Local Variables ***/
-
-		HBox buttonPane = new HBox();
-		
-		ArrayList<Button> buttonList = new ArrayList<Button>();
-		
-		Button btnClear   = new Button("Clear"   );
-		Button btnNewUser = new Button("New User");
-		Button btnUndo    = new Button("Undo"    );
-		Button btnRedo    = new Button("Redo"    );
-		Button btnLoad    = new Button("Load"    );
-		Button btnExport  = new Button("Export"  );
-		Button btnExit    = new Button("Exit"    );
-		
-		/*** Add buttons to Array ***/
-		
-		buttonList.add(btnClear  );
-		buttonList.add(btnNewUser);
-		buttonList.add(btnUndo   );
-		buttonList.add(btnRedo   );
-		buttonList.add(btnLoad   );
-		buttonList.add(btnExport );
-		buttonList.add(btnExit   );	
-		
-		/*** Set spacing for buttons ***/
-		
-		buttonPane.setPadding(new Insets(0, 15, 0, 15));
-		buttonPane.setSpacing(BUTTON_SPACING);
-		
-		/*** Set button size and add to pane***/
-		
-		for (Button b : buttonList) {
-			
-			b.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-			
-			buttonPane.getChildren().add(b);
-		}
-		
-		return buttonPane;
-	}
-	
-	private HBox createPlaceHolder() {
-		
-		/*** Local Variables ***/
-		
-		HBox spacer = new HBox();
-		
-		spacer.setSpacing(20);
-		
-		for (int i = 0; i < 7; i++) {
-			spacer.getChildren().add(new Button("Placeholder " + i));
-		}		
-		
-		return spacer;
-	}
-	
-	private HBox createTopPanel() { 
-		
-		HBox topPanel = new HBox();
-		
-		// create radio buttons for left-most feature
-    	RadioButton rb1 = new RadioButton("Show all friends");
-    	RadioButton rb2 = new RadioButton("Show mutual friends");
-    	RadioButton rb3 = new RadioButton("Show shortest path");
-
-        VBox tGroupContainer = new VBox(rb1, rb2, rb3);
-    	final ToggleGroup tGroup = new ToggleGroup();
-    	
-    	rb1.setSelected(true);
-    	rb1.setToggleGroup(tGroup);
-    	rb2.setToggleGroup(tGroup);
-    	rb3.setToggleGroup(tGroup);
-    	
-    	// create combo box for main profile
-    	VBox v1 = new VBox();
-    	Label l1 = new Label("Main Profile");
-    	ObservableList<String> users = 
-    			FXCollections.observableArrayList("User 1","User 2","User 3");
-    	ComboBox c1 = new ComboBox(users);
-    	v1.getChildren().addAll(l1, c1);
-    	
-    	// create combo box for friend
-    	VBox v2 = new VBox();
-    	Label l2 = new Label("Main Profile");
-    	ObservableList<String> friends = 
-    			FXCollections.observableArrayList("Friend 1","Friend 2","Friend 3");
-    	ComboBox c2 = new ComboBox(users);
-    	v2.getChildren().addAll(l2, c2);
-    	
-    	// create button for remove user and remove friendship
-		Button btnRmUser = new Button("Remove User");
-    	Button btnRmFriend = new Button("Remove Friendship");
-		
-		final double BUTTON_HEIGHT  = 20.0;
-		final double BUTTON_WIDTH   = 90.0;
-		
-		btnRmUser.setPrefSize(150.0, 20.0);
-		btnRmFriend.setPrefSize(150.0, 20.0);
-    	
-    	// add all nodes to top panel
-    	topPanel.getChildren().addAll(tGroupContainer, v1, v2, btnRmUser, btnRmFriend);
-		topPanel.setSpacing(20);
-
-		return topPanel;
-		
-	}
-	
-	private HBox createBottomPanel() { 
-		
-		HBox bottomPanel = new HBox();
-				
-		String prevAction = "Last Action Taken";
-		Label lastAction = new Label("Last Action Taken");
-		
-		int currentUsers = 0;
-		Label userCount = new Label("Total Current Users: " + currentUsers);
-		
-		bottomPanel.getChildren().addAll(lastAction, userCount);
-		bottomPanel.setSpacing(475);
-		
-		return bottomPanel;
-		
-	}
+	/*** Application ***/
 	
 	public static void main(String[] args) {
 		launch(args);

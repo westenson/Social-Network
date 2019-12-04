85fd67f66dd7144492140dc19d688eaee8ee58cc
DrewZ24 <drzimmerman3@wisc.edu>
2019-12-03 20:24:09 -0600

    a2 Interfaces

---
+++ aTeam-CS400/application/Graph.java
@@ -0,0 +1,49 @@
+package application;
+
+import java.util.Set;
+
+public class Graph implements GraphADT {
+
+  @Override
+  public boolean addEdge(Person user1, Person user2) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean removeEdge(Person user1, Person user2) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean addNode(Person user) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean removeNode(Person user) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public Set<Person> getNeighbors(Person user) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Person getNode(Person user) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Set<Person> getAllNodes() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}

c72ed2941b6477bca51fe2139de4a734cb416ca8
westenson <35500498+westenson@users.noreply.github.com>
2019-12-04 11:18:11 -0600

    Create Person.java

---
+++ aTeam-CS400/application/Person.java
@@ -0,0 +1,52 @@
+package application;
+
+/***************************************************************************************************
+ *
+ * @author Wally Estenson
+ *
+ * @version 1.0
+ *          <p>
+ *
+ *          File: Person.java
+ *          <p>
+ *
+ *          Date: December 4, 2019
+ *          <p>
+ *
+ *          Purpose: aTeam p3 - data structure implementation
+ *
+ *          Description: A person object represents an individual in the social
+ *          network. They have a name an array of the names of other persons who
+ *          have been specified as their friends.
+ *
+ *          Comment:
+ *
+ ***************************************************************************************************/
+public class Person {
+    private String name;
+    private String[] friends;
+
+    public Person() {
+    }
+
+    public Person(String name, String[] friends) {
+        this.name = name;
+        this.friends = friends;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String[] getFriends() {
+        return this.friends;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFriends(String[] friends) {
+        this.friends = friends;
+    }
+}

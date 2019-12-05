6e0dac1580e352a8088346a367da4b55d74a86af
westenson <35500498+westenson@users.noreply.github.com>
2019-12-04 19:22:34 -0600

    Updated graph, person, and social network classes

---
+++ aTeam_GUI/application/GraphADT.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.util.Set;
+
+/**
+ * This interface is for the graph.
+ * 
+ * @author Drew Zimmerman
+ *
+ */
+public interface GraphADT {
+  /**
+   * Adds an edge between two users.
+   * 
+   * @param user1 The first friend.
+   * @param user2 The second friend used to create the edge.
+   * @return true if edge creation was successful, else false.
+   */
+  public boolean addEdge(Person user1, Person user2);
+
+  /**
+   * Removes an edge between two friends.
+   * 
+   * @param user1 The first user.
+   * @param user2 The second user.
+   * @return true is successfully deleted, else false.
+   */
+  public boolean removeEdge(Person user1, Person user2);
+
+  /**
+   * Adds a user to the graph.
+   * 
+   * @param user the user to be added.
+   * @return true if user was added.
+   */
+  public boolean addNode(Person user);
+
+  /**
+   * Removes a user from the graph.
+   * 
+   * @param user the user to be removed.
+   * @return true if the user was removed.
+   */
+  public boolean removeNode(Person user);
+
+  /**
+   * Gets the neighbors of a user in the graph.
+   * 
+   * @param user the user to retrieve the neighbors from.
+   * @return the neighbors of the user.
+   */
+  public Set<Person> getNeighbors(Person user);
+
+  /**
+   * Get a user from the graph.
+   * 
+   * @param user the user to get from.
+   * @return the user person from the graph.
+   */
+  public Person getNode(Person user);
+
+  /**
+   * Gets all the users from the graph.
+   * 
+   * @return every user from the graph.
+   */
+  public Set<Person> getAllNodes();
+
+}
---
+++ aTeam_GUI/application/SocialNetworkADT.java
@@ -0,0 +1,86 @@
+package application;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This interface is for the social network visualization.
+ * 
+ * @author Drew Zimmerman
+ *
+ */
+public interface SocialNetworkADT {
+
+  /**
+   * Adds a friend to the social network.
+   * 
+   * @param friend1 One of the friends.
+   * @param friend2 The second friend to be added to the friendship.
+   * @return true if friends were added.
+   */
+  public boolean addFriends(String friend1, String friend2);
+
+  /**
+   * Removes a friend from the network.
+   * 
+   * @param friend1 The first friend.
+   * @param friend2 The second friend to remove the friendship.
+   * @return true if friends were removed.
+   */
+  public boolean removeFriends(String friend1, String friend2);
+
+  /**
+   * Adds a new user to the network.
+   * 
+   * @param user The user to be added.
+   * @return true if user was added.
+   */
+  public boolean addUser(String user);
+
+  /**
+   * Removes a user from the network.
+   * 
+   * @param user The user to be removed.
+   * @return true is friend was removed.
+   */
+  public boolean removeUser(String user);
+
+  /**
+   * Gets the friends of a user.
+   * 
+   * @param user the user to figure out the friends of.
+   * @return the users friends.
+   */
+  public Set<Person> getFriend(String user);
+
+  /**
+   * Gets the shared friendships between two users.
+   * 
+   * @param friend1 one of the friends.
+   * @param friend2 the other friend to be compared.
+   * @return the mutual friends between the two users.
+   */
+  public Set<Person> getMutualFriends(String friend1, String friend2);
+
+  /**
+   * Gets the shortest path between two users
+   * 
+   * @param user1 The first user
+   * @param user2 The user to be compared.
+   * @return the shortest path between the two users.
+   */
+  public List<Person> getShortestPath(String user1, String user2);
+
+  /**
+   * Gets the connected components.
+   * 
+   * @return the connected components.
+   */
+  public Set<Graph> getConnectedComponents();
+
+  public void loadFromFile(File file);
+
+  public void saveToFile(File file);
+
+}

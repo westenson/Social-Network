ee33997a87b214277f39e5a48c1d05de1c085003
westenson <35500498+westenson@users.noreply.github.com>
2019-12-10 16:57:13 -0600

    Socialnetwork and socialnetworktest classes cleanup

--- application/SocialNetwork.java
+++ application/SocialNetwork.java
@@ -19,18 +19,26 @@ import java.util.Set;
  *
  * @author Wally Estenson, Cela Peterson
  *
- * @version 1.0
+ * @version 1.3
  *          <p>
  *
  *          File: SocialNetwork.java
  *          <p>
  *
- *          Date: December 9, 2019
+ *          Date: December 10, 2019
  *          <p>
  *
  *          Purpose: aTeam p3 - data structure implementation
  *
- *          Description: Social network adt that utilizes the graph.
+ *          Description: Social network ADT that utilizes the graph.java class.
+ *          Class enables user to call all methods within the graph class to
+ *          create the social network (adding and removing users, adding and
+ *          removing friends, and getting a list of a user's friends). Further
+ *          this class, has methods with new functionality that allow the user
+ *          to get mutual friends between two users, get the shortest path
+ *          between two users, get the connected components within the network
+ *          (friend groups), use an external file to create the social network,
+ *          and export a file with the social network.
  *
  *          Comment:
  *
@@ -41,19 +49,54 @@ public class SocialNetwork implements SocialNetworkADT {
 	private Graph graph;
 	private int numberOfConnectedComponents;
 
-	private int size;// number of edges
-	private int order;// number of users
-
+	/**
+	 * Social network constructor. Creates an instance of graph.
+	 */
 	public SocialNetwork() {
 		graph = new Graph();
 		numberOfConnectedComponents = 0;
 	}
 
+	/**
+	 * Creates a new user and calls the graph class to add that user as a new
+	 * node in the graph
+	 * 
+	 * @param String user to be added
+	 * @return true is user is added
+	 */
+	@Override
+	public boolean addUser(String user) {
+		Person user1 = new Person(user);
+		return graph.addNode(user1);
+	}
+
+	/**
+	 * Removes a user from the social network by calling the graph class method
+	 * to remove a node
+	 * 
+	 * @param String user to be removed
+	 * @return true is user is removed
+	 */
+	@Override
+	public boolean removeUser(String user) {
+		return graph.removeNode(graph.getNode(user));
+	}
+
+	/**
+	 * Adds a friendship/edge between two users. If either of the input users do
+	 * not yet exist, add them first by calling the method in the graph class to
+	 * add a new node.
+	 * 
+	 * @param String friend1 and String friend2
+	 * @return true if friendship is added
+	 */
 	@Override
 	public boolean addFriends(String friend1, String friend2) {
 		Person user1 = new Person(friend1);
 		Person user2 = new Person(friend2);
 
+		// if either of the input friends do not yet exist, add them as new
+		// nodes
 		if (graph.getNode(friend1) == null)
 			graph.addNode(user1);
 		if (graph.getNode(friend2) == null)
@@ -62,60 +105,76 @@ public class SocialNetwork implements SocialNetworkADT {
 		return graph.addEdge(graph.getNode(friend1), graph.getNode(friend2));
 	}
 
+	/**
+	 * Call the graph method to remove a friendship/edge between two users
+	 * 
+	 * @param String friend1 and String friend2
+	 * @return true if friendship is removed
+	 */
 	@Override
 	public boolean removeFriends(String friend1, String friend2) {
 		return graph.removeEdge(graph.getNode(friend1), graph.getNode(friend2));
 	}
 
-	@Override
-	public boolean addUser(String user) {
-		Person user1 = new Person(user);
-		return graph.addNode(user1);
-	}
-
-	@Override
-	public boolean removeUser(String user) {
-		return graph.removeNode(graph.getNode(user));
-	}
-
+	/**
+	 * Call the graph method to get the friends of the specified user
+	 * 
+	 * @param String user
+	 * @return Set of friends of the user
+	 */
 	@Override
 	public Set<Person> getFriends(String user) {
-
 		return graph.getNeighbors(graph.getNode(user));
 	}
 
+	/**
+	 * Method to get the mutual friends between two users. Utilizes the
+	 * getNeighbors method in the graph class for both users and compares these
+	 * sets to find mutual friends.
+	 * 
+	 * @param String friend1 and String friend2
+	 * @return Set of mutual friends
+	 */
 	@Override
 	public Set<Person> getMutualFriends(String friend1, String friend2) {
 
+		// create two sets of the specified users' friends
 		Set<Person> user1Neighbors = graph.getNeighbors(graph.getNode(friend1));
 		Set<Person> user2Neighbors = graph.getNeighbors(graph.getNode(friend2));
 
+		// create a third set that we will add mutual friends to
 		Set<Person> mutualFriends = new HashSet<Person>();
 
 		Iterator<Person> iterator = user1Neighbors.iterator();
 
+		// iterate through the first user's friends
 		while (iterator.hasNext()) {
+			// create a new person object of the first user's friend
 			Person compare1 = iterator.next();
 
 			Iterator<Person> iterator2 = user2Neighbors.iterator();
 
+			// iterate through the second user's friends
 			while (iterator2.hasNext()) {
-
+				// create a new person object of the second user's friend
 				Person compare2 = iterator2.next();
 
+				// compare the friends, if its a match, add to the mutual
+				// friends set
 				if (compare1.getName().equals(compare2.getName())) {
 					mutualFriends.add(compare2);
-				} else {
-
 				}
-
 			}
-
 		}
 		return mutualFriends;
 	}
 
-	@SuppressWarnings("unchecked")
+	/**
+	 * Method to get the shortest path between two users
+	 * 
+	 * @param String user1 and String user2
+	 * @return List of the person/s that are the shortest path between the users
+	 */
 	@Override
 	public List<Person> getShortestPath(String user1, String user2) {
 
@@ -195,21 +254,22 @@ public class SocialNetwork implements SocialNetworkADT {
 		return shortestPath;
 	}
 
-	// work through each user in the graph
-	// for the first user, add all friends to first new graph
-	// then for each of the friends, add their friends to the first graph
-	// mark all the user nodes of the initial friends as visited
-	// do again on the first graph until no new friends are shown
-	// then move on to the next unvisited node in all nodes
+	/**
+	 * Method that returns a set of graphs representing the connected components
+	 * of the social network.
+	 * 
+	 * @return Set of graphs of the connected components
+	 */
 	@Override
 	public Set<Graph> getConnectedComponents() {
+		// must rest the number of connected components each time its called
+		numberOfConnectedComponents = 0;
 
-		Set<Graph> connectedComponents = new HashSet<Graph>();// set containing
-																// graphs of the
-		// connected groups
+		// set that we will be adding the connected components to
+		Set<Graph> connectedComponents = new HashSet<Graph>();
 
-		Set<Person> allUsers = graph.getAllNodes(); // set containing all users
-													// in the network
+		// set containing all the users in the network
+		Set<Person> allUsers = graph.getAllNodes();
 
 		Iterator<Person> usersIterator = allUsers.iterator();
 		Iterator<Person> visitedReset = allUsers.iterator();
@@ -236,12 +296,16 @@ public class SocialNetwork implements SocialNetworkADT {
 		return connectedComponents;
 	}
 
-	// BFS to return a graph representing a users network, including all path
-	// ie the returned graph will not include users that the specified user does
-	// not have a path to
+	/**
+	 * Breadth First Search method that returns a graph representing a users
+	 * network, including all paths ie. the returned graph will not include
+	 * users that the specififed user does not have a path to
+	 * 
+	 * @return Graph of users network
+	 */
 	private Graph BFSearch(Person user) {
 
-		Graph graph1 = new Graph();
+		Graph usersNetwork = new Graph();
 
 		// Create a queue for BFS
 		LinkedList<Person> queue = new LinkedList<Person>();
@@ -255,29 +319,42 @@ public class SocialNetwork implements SocialNetworkADT {
 
 			// Dequeue the user at the front and add them to the new graph
 			user = queue.poll();
-			graph1.addNode(user);
+			usersNetwork.addNode(user);
 
-			// Get all the users friends, add edges between them and the user,
-			// and add them to the queue
 			Iterator<Person> friendsIterator = graph.getNeighbors(user)
 					.iterator();
+
+			// Get all the users friends, add edges between them and the
+			// user,and add them to the queue
 			while (friendsIterator.hasNext()) {
 				Person friend = friendsIterator.next();
 				if (!friend.getVisited()) {
 					friend.setVisited(true);
-					graph1.addEdge(user, friend);
+					usersNetwork.addEdge(user, friend);
 					queue.add(friend);
 				}
 			}
 		}
-		return graph1;
+		return usersNetwork;
 	}
 
+	/**
+	 * Gets the number of connected components in the social network. Must call
+	 * the getConnectedComponents method first which does the calculation.
+	 * 
+	 * @return int number of connected components 
+	 */
 	@Override
 	public int getNumberOfConnectedComponents() {
+		getConnectedComponents();
 		return numberOfConnectedComponents;
 	}
 
+	/**
+	 * Takes file and builds social network 
+	 * 
+	 * @param file 
+	 */
 	@Override
 	public void loadFromFile(File file) {
 		Scanner scnr = null;
@@ -304,7 +381,11 @@ public class SocialNetwork implements SocialNetworkADT {
 		scnr.close();
 	}
 
-	// Use getConnectedComponents method
+	/**
+	 * Exports social network to a specified file 
+	 * 
+	 * @param file 
+	 */
 	@Override
 	public void saveToFile(File file) {
 
@@ -345,64 +426,45 @@ public class SocialNetwork implements SocialNetworkADT {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
-	// main method for testing purposes
-	public static void main(String[] args) {
-		SocialNetwork socialNetwork = new SocialNetwork();
-		Person wally = new Person("wally");
-		Person jack = new Person("jack");
-		Person bill = new Person("bill");
-		Person jake = new Person("jake");
-
-		socialNetwork.graph.addNode(wally);
-		socialNetwork.graph.addNode(jack);
-		socialNetwork.graph.addNode(bill);
-		socialNetwork.graph.addNode(jake);
-
-		socialNetwork.graph.addEdge(wally, jack);
-		socialNetwork.graph.addEdge(bill, jake);
-		socialNetwork.graph.addEdge(wally, jake);
-		socialNetwork.graph.addEdge(wally, bill);
-
-		System.out.println(socialNetwork.graph);
-
-		socialNetwork.graph.removeEdge(jack, wally);
-		System.out.println(socialNetwork.graph);
-
-		// socialNetwork.graph.removeNode(wally);
-
-		// System.out.println(socialNetwork.graph.order());
-		// System.out.println(socialNetwork.graph.size());
-		System.out.println(
-				socialNetwork.getMutualFriends("jake", "bill").toString());
-		System.out.println(socialNetwork.getConnectedComponents().toString());
-		// System.out.print(socialNetwork.graph);
-
-	}
-
-	*/
-	
-	// size = number of edges/friendships
+	 * Returns number of edges in the social network
+	 * 
+	 * @param int size
+	 */
 	public int size() {
 		return graph.size();
 	}
 
-	// order = number of users/nodes
+	/**
+	 * Returns number of users in the social network
+	 * 
+	 * @param int order
+	 */
 	public int order() {
 		return graph.order();
 	}
 
-	// prints out personsMap for testing purposes
+	/**
+	 * Prints social network
+	 * For testing purposes 
+	 * 
+	 * @return String of social network
+	 */
 	@Override
 	public String toString() {
 		return graph.toString();
 
 	}
 
-	// gets user for testing purposes 
+	/**
+	 * Gets person object 
+	 * For testing purposes 
+	 * 
+	 * @param String user
+	 * @return Person user 
+	 */
 	public Person getUser(String user) {
 		return graph.getNode(user);
 
--- application/SocialNetworkTest.java
+++ application/SocialNetworkTest.java
@@ -10,13 +10,30 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author Andrew Irvine
- *
- */
+/***************************************************************************************************
+*
+* @author Andrew Irvine, Wally Estenson
+*
+* @version 1.5
+*          <p>
+*
+*          File: SocialNetorkTest.java
+*          <p>
+*
+*          Date: December 10, 2019
+*          <p>
+*
+*          Purpose: aTeam p3 - data structure implementation testing
+*
+*          Description: Testing of the socialNetwork class 
+*
+*          Comment:
+*
+***************************************************************************************************/
+
 class SocialNetworkTest {
 
-	public static SocialNetwork graphInstance;
+	public static SocialNetwork socialNetworkInstance;
 
 	/*** Setup ***/
 
@@ -26,45 +43,45 @@ class SocialNetworkTest {
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		graphInstance = new SocialNetwork();
+		socialNetworkInstance = new SocialNetwork();
 	}
 
 	@AfterEach
 	public void tearDown() throws Exception {
-		graphInstance = null;
+		socialNetworkInstance = null;
 	}
 
 	/*** Tests ***/
 
 	@Test
 	public void test00_NewSocialNetworkIsEmpty() {
-		assertEquals(0, graphInstance.order());
-		assertEquals(0, graphInstance.size());
+		assertEquals(0, socialNetworkInstance.order());
+		assertEquals(0, socialNetworkInstance.size());
 	}
 
 	@Test
 	public void test01_add01User_OrderEquals01() {
-		graphInstance.addUser("user");
-		assertEquals(1, graphInstance.order());
+		socialNetworkInstance.addUser("user");
+		assertEquals(1, socialNetworkInstance.order());
 	}
 
 	@Test
 	public void test02_add10Users_OrderEquals10() {
 		for (int i = 0; i < 10; i++) {
-			graphInstance.addUser("user" + i);
+			socialNetworkInstance.addUser("user" + i);
 		}
-		assertEquals(10, graphInstance.order());
+		assertEquals(10, socialNetworkInstance.order());
 	}
 
 	@Test
 	public void test03_add01User_remove01User_OrderEquals00() {
 		String user = "testUser";
 
-		graphInstance.addUser(user);
-		assertEquals(1, graphInstance.order());
-		graphInstance.removeUser(user);
+		socialNetworkInstance.addUser(user);
+		assertEquals(1, socialNetworkInstance.order());
+		socialNetworkInstance.removeUser(user);
 
-		assertEquals(0, graphInstance.order());
+		assertEquals(0, socialNetworkInstance.order());
 	}
 
 	@Test
@@ -72,29 +89,14 @@ class SocialNetworkTest {
 		String user = "testUser";
 
 		for (int i = 0; i < 10; i++) {
-			graphInstance.addUser(user + i);
+			socialNetworkInstance.addUser(user + i);
 		}
-		assertEquals(10, graphInstance.order());
+		assertEquals(10, socialNetworkInstance.order());
 
 		for (int i = 0; i < 10; i++) {
-			graphInstance.removeUser(user + i);
+			socialNetworkInstance.removeUser(user + i);
 		}
-		assertEquals(0, graphInstance.order());
-	}
-
-	@Test
-	public void test06_add10Users_remove10Users_OrderEquals00() {
-		String user = "testUser";
-
-		for (int i = 0; i < 10; i++) {
-			graphInstance.addUser(user + i);
-		}
-		assertEquals(10, graphInstance.order());
-
-		for (int i = 0; i < 10; i++) {
-			graphInstance.removeUser(user + i);
-		}
-		assertEquals(0, graphInstance.order());
+		assertEquals(0, socialNetworkInstance.order());
 	}
 
 	@Test
@@ -102,52 +104,137 @@ class SocialNetworkTest {
 		String user1 = "testUser1";
 		String user2 = "testUser2";
 
-		graphInstance.addFriends(user1, user2);
-		assertEquals(2, graphInstance.order());
-		assertEquals(1, graphInstance.size());
+		socialNetworkInstance.addFriends(user1, user2);
+		assertEquals(2, socialNetworkInstance.order());
+		assertEquals(1, socialNetworkInstance.size());
 	}
 
 	@Test
-	public void test010_AddFriends_RemoveUser() {
-		graphInstance.addUser("testUser3");
-		graphInstance.addFriends("testUser1", "testUser2");
+	public void test06_add10Users_remove10Users_OrderEquals00() {
+		String user = "testUser";
 
-		graphInstance.removeUser("testUser1");
+		for (int i = 0; i < 10; i++) {
+			socialNetworkInstance.addUser(user + i);
+		}
+		assertEquals(10, socialNetworkInstance.order());
 
-		if (graphInstance.getUser("testUser1") != null) {
+		for (int i = 0; i < 10; i++) {
+			socialNetworkInstance.removeUser(user + i);
+		}
+		assertEquals(0, socialNetworkInstance.order());
+	}
+
+	@Test
+	public void test07_AddFriends_RemoveUser() {
+		socialNetworkInstance.addUser("testUser3");
+		socialNetworkInstance.addFriends("testUser1", "testUser2");
+
+		socialNetworkInstance.removeUser("testUser1");
+
+		if (socialNetworkInstance.getUser("testUser1") != null) {
 			fail("Failed to remove user.");
 		}
 	}
 
 	@Test
-	public void test011_AddUser_RemoveUser() {
-		graphInstance.addUser("testUser3");
+	public void test08_AddUser_RemoveUser() {
+		socialNetworkInstance.addUser("testUser3");
 
-		graphInstance.addUser("testUser1");
+		socialNetworkInstance.addUser("testUser1");
 
-		graphInstance.removeUser("testUser1");
+		socialNetworkInstance.removeUser("testUser1");
 
-		if (graphInstance.getUser("testUser1") != null) {
+		if (socialNetworkInstance.getUser("testUser1") != null) {
 			fail("Failed to remove user.");
 		}
 	}
 
 	@Test
-	public void test012_AddUsers_RemoveFriends() {
-		graphInstance.addUser("testUser3");
+	public void test09_AddUsers_RemoveFriends() {
+		socialNetworkInstance.addUser("testUser3");
 
-		graphInstance.addUser("testUser1");
+		socialNetworkInstance.addUser("testUser1");
 
-		graphInstance.addFriends("testUser3", "testUser1");
+		socialNetworkInstance.addFriends("testUser3", "testUser1");
 
-		graphInstance.removeFriends("testUser3", "testUser1");
+		socialNetworkInstance.removeFriends("testUser3", "testUser1");
 
-		if (!graphInstance.getFriends("testUser1").isEmpty()) {
+		if (!socialNetworkInstance.getFriends("testUser1").isEmpty()) {
 			fail("Failed to remove edge.");
 		}
-		if (!graphInstance.getFriends("testUser3").isEmpty()) {
+		if (!socialNetworkInstance.getFriends("testUser3").isEmpty()) {
 			fail("Failed to remove edge.");
 		}
 	}
 
+	@Test
+	public void test010_GetMutualFriends() {
+		socialNetworkInstance.addUser("1");
+		socialNetworkInstance.addUser("2");
+		socialNetworkInstance.addUser("3");
+		socialNetworkInstance.addUser("4");
+
+		socialNetworkInstance.addFriends("1", "2");
+		socialNetworkInstance.addFriends("3", "2");
+		socialNetworkInstance.addFriends("3", "1");
+
+		if (!socialNetworkInstance.getMutualFriends("1", "2")
+				.contains(socialNetworkInstance.getUser("3"))) {
+			fail("User 3 should be a mutual friend of 1 and 2");
+		}
+		if (socialNetworkInstance.getMutualFriends("1", "2")
+				.contains(socialNetworkInstance.getUser("4"))) {
+			fail("User 4 should not be a mutual friend of 1 and 2");
+		}
+	}
+
+	@Test
+	public void test011_GetShortestPath() {
+		socialNetworkInstance.addUser("1");
+		socialNetworkInstance.addUser("2");
+		socialNetworkInstance.addUser("3");
+		socialNetworkInstance.addUser("4");
+
+		socialNetworkInstance.addFriends("1", "2");
+		socialNetworkInstance.addFriends("3", "2");
+		socialNetworkInstance.addFriends("3", "1");
+		socialNetworkInstance.addFriends("3", "4");
+
+		// Shortest path should contain 3,1,4 and not 2
+		if (!socialNetworkInstance.getShortestPath("1", "4")
+				.contains(socialNetworkInstance.getUser("3"))) {
+			fail("Incorrect Shortest Path (1)");
+		}
+		if (!socialNetworkInstance.getShortestPath("1", "4")
+				.contains(socialNetworkInstance.getUser("1"))) {
+			fail("Incorrect Shortest Path (2)");
+		}
+		if (!socialNetworkInstance.getShortestPath("1", "4")
+				.contains(socialNetworkInstance.getUser("4"))) {
+			fail("Incorrect Shortest Path (3)");
+		}
+		if (socialNetworkInstance.getShortestPath("1", "4")
+				.contains(socialNetworkInstance.getUser("2"))) {
+			fail("Incorrect Shortest Path (4)");
+		}
+	}
+
+	@Test
+	public void test012_connectedComponentsNumber() {
+		socialNetworkInstance.addUser("1");
+		socialNetworkInstance.addUser("2");
+		socialNetworkInstance.addUser("3");
+		socialNetworkInstance.addUser("4");
+		socialNetworkInstance.addUser("5");
+		socialNetworkInstance.addUser("6");
+
+		socialNetworkInstance.addFriends("1", "2");
+		socialNetworkInstance.addFriends("3", "2");
+		socialNetworkInstance.addFriends("3", "1");
+		socialNetworkInstance.addFriends("3", "4");
+
+		if (socialNetworkInstance.getNumberOfConnectedComponents() != 3) {
+			fail("Number of connected components should be 3");
+		}
+	}
 }

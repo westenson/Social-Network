93d01bab17fc543a2cf378e8966ea99098c3aef8
westenson <35500498+westenson@users.noreply.github.com>
2019-12-10 10:43:15 -0600

    Graph test class updates

--- application/GraphTest.java
+++ application/GraphTest.java
@@ -9,10 +9,27 @@ import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * @author airvi
- *
- */
+/***************************************************************************************************
+*
+* @author Andrew Irvine, Wally Estenson
+*
+* @version 1.3
+*          <p>
+*
+*          File: GraphTest.java
+*          <p>
+*
+*          Date: December 10, 2019
+*          <p>
+*
+*          Purpose: aTeam p3 - data structure implementation testing
+*
+*          Description: Testing of the graph class 
+*
+*          Comment:
+*
+***************************************************************************************************/
+
 class GraphTest {
 	
 	public static Graph graphInstance;
@@ -84,45 +101,77 @@ class GraphTest {
 		assertEquals(1, graphInstance.size());	
 	}
 	
-/**
- * Must fix test still
+
 	@Test
 	public void test05_Add05Edges_GetNeighbors() {
 		Person Person1 =new Person ("testUser1");
-		Person target = new Person ("testEdge");
-		List<String>neighbors = new ArrayList<String>();
-		for (int i=0; i<5; i++) {
-			graphInstance.addEdge(Person1, target);
-			neighbors.add(edges.getTarget());
+		Person neighbor1 = new Person ("testEdge");
+		Person neighbor2 = new Person ("testEdge");
+
+
+		graphInstance.addEdge(Person1, neighbor1);
+		graphInstance.addEdge(Person1, neighbor2);
+		
+		
+		Set<Person> neighbors = graphInstance.getNeighbors(Person1);
+		
+		if (!neighbors.contains(neighbor1)) {
+			fail("Improper set returned: " + graphInstance.getNeighbors(Person1) + "\n" +  "expected set: " + neighbors);	
 		}
-		if (!neighbors.equals(graphInstance.getNeighbors(Person1))) {
+		if (!neighbors.contains(neighbor2)) {
 			fail("Improper set returned: " + graphInstance.getNeighbors(Person1) + "\n" +  "expected set: " + neighbors);	
 		}
 	}
-**/
+
 	
-//Must finish Test	
+	//adds person1 node with neighbors. Then removes person one and checks that neighbors still dont have person1 as a friend
 	@Test
 	public void test06_RemoveNode_RemovesEdgeBetweenNodes() {
+		Person Person1 =new Person ("testUser1");
+		Person neighbor1 = new Person ("testEdge");
+		Person neighbor2 = new Person ("testEdge");
 		
+
+		graphInstance.addEdge(Person1, neighbor1);
+		graphInstance.addEdge(Person1, neighbor2);
+		
+		graphInstance.removeNode(Person1);
+		
+		Set<Person> neighbors1 = graphInstance.getNeighbors(neighbor1);
+		Set<Person> neighbors2 = graphInstance.getNeighbors(neighbor2);
+
+		if (neighbors1.contains(Person1)) {
+			fail("Failed to remove edge between users when user was removed.");	
+		}
+		if (neighbors2.contains(Person1)) {
+			fail("Failed to remove edge between users when user was removed.");	
+		}
 	}
 	
 	@Test
 	public void test07_Add05Edges_Remove05Edges_SizeEquals05_OrderEquals00() {
 		Person Person1 = new Person("testUser");
-		//Person Person2 = new Person("testUser2");
+		Person Person2 = new Person("testUser");
+		Person Person3 = new Person("testUser");
+		Person Person4 = new Person("testUser");
+		Person Person5 = new Person("testUser");
+		Person Person6 = new Person("testUser");
+
+		graphInstance.addEdge(Person1, Person2);
+		graphInstance.addEdge(Person1, Person3);
+		graphInstance.addEdge(Person1, Person4);
+		graphInstance.addEdge(Person1, Person5);
+		graphInstance.addEdge(Person1, Person6);
 		
-		for (int i = 0; i<5; i++) {
-			graphInstance.addEdge(Person1, new Person("testUser" + i));
-		}
 		assertEquals(6, graphInstance.order());
 		assertEquals(5, graphInstance.size());
+
+		graphInstance.removeEdge(Person1, Person2);
+		graphInstance.removeEdge(Person1, Person3);
+		graphInstance.removeEdge(Person1, Person4);
 		
-		for (int i = 0; i<5; i++) {
-			graphInstance.removeEdge(Person1, new Person("testUser" + i));
-		}
 		assertEquals(6,graphInstance.order());
-		assertEquals(0, graphInstance.size());
+		assertEquals(2, graphInstance.size());
 	}
 
 	@Test
@@ -139,8 +188,5 @@ class GraphTest {
 		
 		assertEquals(0,graphInstance.order());
 		assertEquals(0,graphInstance.size());
-	}
-	
-
-	
+	}	
 }

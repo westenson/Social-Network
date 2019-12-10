b58db7ddcb05aecb6c6201053026eb2268d1bc76
westenson <35500498+westenson@users.noreply.github.com>
2019-12-10 16:11:16 -0600

    Graph and graphtest classes cleanup

--- application/Graph.java
+++ application/Graph.java
@@ -12,18 +12,18 @@ import java.util.Set;
  *
  * @author Wally Estenson
  *
- * @version 1.0
+ * @version 1.4
  *          <p>
  *
  *          File: Graph.java
  *          <p>
  *
- *          Date: December 4, 2019
+ *          Date: December 10, 2019
  *          <p>
  *
  *          Purpose: aTeam p3 - data structure implementation
  *
- *          Description: Undirected and unweighted graph implementation for the
+ *          Description: Undirected and un-weighted graph implementation for the
  *          social network. Graph nodes are person objects.
  *
  *          Comment:
@@ -50,66 +50,6 @@ public class Graph implements GraphADT {
 					+ (personsMap.get(v)).toString());
 
 		return s.toString();
-	}
-
-	/**
-	 * Add the edge between user1 to user2 to this graph. (edge is undirected
-	 * and unweighted) If either user does not exist, add vertex, and add edge,
-	 * no exception is thrown. If the edge exists in the graph, no edge is added
-	 * and no exception is thrown.
-	 * 
-	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are
-	 * in the graph 3. the edge is not in the graph
-	 * 
-	 * @param user1
-	 */
-	@Override
-	public boolean addEdge(Person user1, Person user2) {
-		// if the inputs are null, cannot add edge
-		if (user1 == (null) || user2 == (null))
-			return false;
-
-		// if either of the users do not already exist, add them as new persons
-		if (!personsMap.containsKey(user1))
-			this.addNode(user1);
-
-		if (!personsMap.containsKey(user2))
-			this.addNode(user2);
-
-		// add new edge to user1
-		personsMap.get(user1).add(user2);
-		personsMap.get(user2).add(user1);
-		size++;
-		return true;
-	}
-
-	/**
-	 * Remove the edge between user1 and user2. (edge is undirected and
-	 * unweighted so we remove from both users) If either vertex does not exist,
-	 * or if an edge from vertex1 to vertex2 does not exist, no edge is removed
-	 * and no exception is thrown.
-	 * 
-	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are
-	 * in the graph 3. the edge from vertex1 to vertex2 is in the graph
-	 */
-	@Override
-	public boolean removeEdge(Person user1, Person user2) {
-		// if either input is null, cannot remove edge
-		if (user1 == (null) || user2 ==(null)) {
-			System.out.print("null");
-			return false;
-		}
-		
-		// if either input is not in the network, cannot remove edge
-		if (!personsMap.containsKey(user1)|| !personsMap.containsKey(user2)) {
-			return false;
-		}
-
-		// remove the edges
-		personsMap.get(user1).remove(user2);
-		personsMap.get(user2).remove(user1);
-		size--;
-		return true;
 	}
 
 	/**
@@ -158,8 +98,11 @@ public class Graph implements GraphADT {
 	@Override
 	public boolean removeNode(Person user) {
 
+		if (user == null)
+			return false;
+
 		// if parameter is an existing person, begin removal process
-		if (personsMap.containsKey(user)) {
+		else if (personsMap.containsKey(user)) {
 
 			// create set of neighbors and iterator so we can remove the edges
 			Set<Person> userNeighbors = getNeighbors(user);
@@ -173,12 +116,77 @@ public class Graph implements GraphADT {
 
 			// remove the user from the social network
 			personsMap.remove(user);
-			order--;
+			order--; // decrement the number of edges in the graph
 			return true;
 		}
 
+		// return false if the user does not exist
 		else
 			return false;
+	}
+
+	/**
+	 * Add the edge between user1 and user2 in the graph. (edge is undirected
+	 * and un-weighted) If either user does not exist, add vertex, and add edge,
+	 * no exception is thrown. If the edge exists in the graph, no edge is added
+	 * and no exception is thrown.
+	 * 
+	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are
+	 * in the graph 3. the edge is not in the graph
+	 * 
+	 * @param user1 and user2
+	 * @return true if the the edge is added between the users
+	 */
+	@Override
+	public boolean addEdge(Person user1, Person user2) {
+		// if the inputs are null, cannot add edge
+		if (user1 == (null) || user2 == (null))
+			return false;
+
+		// if either of the users do not already exist, add them as new persons
+		// first
+		if (!personsMap.containsKey(user1))
+			this.addNode(user1);
+		if (!personsMap.containsKey(user2))
+			this.addNode(user2);
+
+		// add new edge between the users ie. add user1 and user2 to eachother's
+		// friendlists
+		personsMap.get(user1).add(user2);
+		personsMap.get(user2).add(user1);
+
+		size++; // increment number of edges in the graph
+		return true;
+	}
+
+	/**
+	 * Remove the edge between user1 and user2. (edge is undirected and
+	 * un-weighted so we remove from both users) If either vertex does not
+	 * exist, or if an edge from vertex1 to vertex2 does not exist, no edge is
+	 * removed and no exception is thrown.
+	 * 
+	 * Valid argument conditions: 1. neither vertex is null 2. both vertices are
+	 * in the graph 3. the edge from vertex1 to vertex2 is in the graph
+	 * 
+	 * @param user1 and user2
+	 * @return true if edge is removed
+	 */
+	@Override
+	public boolean removeEdge(Person user1, Person user2) {
+		// if either input is null, cannot remove edge
+		if (user1 == (null) || user2 == (null))
+			return false;
+
+		// if either input is not in the network, cannot remove edge
+		if (!personsMap.containsKey(user1) || !personsMap.containsKey(user2))
+			return false;
+
+		// remove the edges, ie remove eachother from friendlists
+		personsMap.get(user1).remove(user2);
+		personsMap.get(user2).remove(user1);
+
+		size--; // decrement number of edges
+		return true;
 	}
 
 	/**
@@ -186,15 +194,14 @@ public class Graph implements GraphADT {
 	 * no need to worry about inbound and outbound
 	 * 
 	 * @Param person that we'll be getting the neighbors of
-	 * @Return list of neighbors
+	 * @Return Set of neighbors
 	 */
 	@Override
 	public Set<Person> getNeighbors(Person user) {
 		Set<Person> neighbors = new HashSet<Person>();
-		
 
 		// iterate through the users neighbors and add them to the neighbors
-		// list
+		// set
 		for (Person friend : personsMap.get(user))
 			neighbors.add(friend);
 
@@ -209,18 +216,20 @@ public class Graph implements GraphADT {
 	 */
 	@Override
 	public Person getNode(String user) {
+		// create set with all nodes and iterator to iterate through that set
 		Set<Person> allNodes = getAllNodes();
 		Iterator<Person> allNodesIterator = allNodes.iterator();
 
+		// iterate through all nodes
 		while (allNodesIterator.hasNext()) {
 			Person user1 = allNodesIterator.next();
 
-			if (user1.getName().equals(user)) {
+			// find the person in the set
+			if (user1.getName().equals(user))
 				return user1;
-			}
-		}
 
-		return null;
+		}
+		return null; // person does not exist
 	}
 
 	/**
@@ -233,7 +242,7 @@ public class Graph implements GraphADT {
 		Set<Person> allUsers = new HashSet<Person>();
 
 		// iterate through all the keys of the social network and add them to
-		// the allUsers list
+		// the allUsers Set
 		for (Person user : personsMap.keySet())
 			allUsers.add(user);
 
--- application/GraphTest.java
+++ application/GraphTest.java
@@ -10,144 +10,162 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
 /***************************************************************************************************
-*
-* @author Andrew Irvine, Wally Estenson
-*
-* @version 1.3
-*          <p>
-*
-*          File: GraphTest.java
-*          <p>
-*
-*          Date: December 10, 2019
-*          <p>
-*
-*          Purpose: aTeam p3 - data structure implementation testing
-*
-*          Description: Testing of the graph class 
-*
-*          Comment:
-*
-***************************************************************************************************/
+ *
+ * @author Andrew Irvine, Wally Estenson
+ *
+ * @version 1.3
+ *          <p>
+ *
+ *          File: GraphTest.java
+ *          <p>
+ *
+ *          Date: December 10, 2019
+ *          <p>
+ *
+ *          Purpose: aTeam p3 - data structure implementation testing
+ *
+ *          Description: Testing of the graph class
+ *
+ *          Comment:
+ *
+ ***************************************************************************************************/
 
 class GraphTest {
-	
+
 	public static Graph graphInstance;
 
-	
-/**Setup**/
+	/** Setup **/
 	@BeforeAll
-	public static void beforeClass() throws Exception{
+	public static void beforeClass() throws Exception {
 	}
-	
-	
+
 	@BeforeEach
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		graphInstance = new Graph();
 	}
-	
+
 	@AfterEach
-	public void tearDown()throws Exception{
+	public void tearDown() throws Exception {
 		graphInstance = null;
 	}
 
-	
-/**Tests for Graph**/			
-	
+	/**
+	 * Check if instance of graph is created and the order and size is 0
+	 */
 	@Test
 	public void test00_EmptyGraph() {
-		assertEquals(0,graphInstance.order());
-		assertEquals(0,graphInstance.size());
+		assertEquals(0, graphInstance.order());
+		assertEquals(0, graphInstance.size());
 	}
-	
+
+	/**
+	 * Check if order correctly equals 1 after inserting 1 node
+	 */
 	@Test
 	public void test01_InsertNode1_OrderEquals1() {
 		Person Person = new Person("user1");
 		graphInstance.addNode(Person);
 		assertEquals(1, graphInstance.order());
 	}
-	
+
+	/**
+	 * Check if order equals 0 after inserting and removing node
+	 */
 	@Test
 	public void test02_AddNode1_RemoveNode1_OrderEquals00() {
-		Person Person1 = new Person ("testUser");
-		
+		Person Person1 = new Person("testUser");
+
 		graphInstance.addNode(Person1);
 		assertEquals(1, graphInstance.order());
 		graphInstance.removeNode(Person1);
 		assertEquals(0, graphInstance.order());
 	}
-	
+
+	/**
+	 * Check if order equals 2 and size equals 1 after inserting two people and
+	 * adding an edge between them
+	 */
 	@Test
 	public void test03_AddEdge_MissingNodesCreated() {
 		Person Person = new Person("testUser1");
-		Person Person1 = new Person ("testUser2");
-		
+		Person Person1 = new Person("testUser2");
+
 		graphInstance.addEdge(Person, Person1);
-		assertEquals(2,graphInstance.order());
-		assertEquals(1,graphInstance.size());
+		assertEquals(2, graphInstance.order());
+		assertEquals(1, graphInstance.size());
 	}
-	
+
+	/**
+	 * Check if adding edge works when one user does not exist Should create
+	 * user and add edge
+	 */
 	@Test
 	public void test04_AddEdge_SingleMissingNodeCreated() {
 		Person Person1 = new Person("testUser1");
 		Person Person2 = new Person("testUser2");
-		
-		assertEquals(0,graphInstance.order());
-		
+
+		assertEquals(0, graphInstance.order());
+
 		graphInstance.addNode(Person1);
 		assertEquals(1, graphInstance.order());
 		graphInstance.addEdge(Person1, Person2);
 		assertEquals(2, graphInstance.order());
-		assertEquals(1, graphInstance.size());	
+		assertEquals(1, graphInstance.size());
 	}
-	
 
+	/**
+	 * Check if adding edges correctly adds to neighbors list
+	 */
 	@Test
-	public void test05_Add05Edges_GetNeighbors() {
-		Person Person1 =new Person ("testUser1");
-		Person neighbor1 = new Person ("testEdge");
-		Person neighbor2 = new Person ("testEdge");
-
+	public void test05_Add02Edges_GetNeighbors() {
+		Person Person1 = new Person("testUser1");
+		Person neighbor1 = new Person("testEdge");
+		Person neighbor2 = new Person("testEdge");
 
 		graphInstance.addEdge(Person1, neighbor1);
 		graphInstance.addEdge(Person1, neighbor2);
-		
-		
+
 		Set<Person> neighbors = graphInstance.getNeighbors(Person1);
-		
+
 		if (!neighbors.contains(neighbor1)) {
-			fail("Improper set returned: " + graphInstance.getNeighbors(Person1) + "\n" +  "expected set: " + neighbors);	
+			fail("Improper set returned: " + graphInstance.getNeighbors(Person1)
+					+ "\n" + "expected set: " + neighbors);
 		}
 		if (!neighbors.contains(neighbor2)) {
-			fail("Improper set returned: " + graphInstance.getNeighbors(Person1) + "\n" +  "expected set: " + neighbors);	
+			fail("Improper set returned: " + graphInstance.getNeighbors(Person1)
+					+ "\n" + "expected set: " + neighbors);
 		}
 	}
 
-	
-	//adds person1 node with neighbors. Then removes person one and checks that neighbors still dont have person1 as a friend
+	/**
+	 * adds person1 node with neighbors. Then removes person one and checks that
+	 * neighbors still don't have person1 as a friend
+	 */
 	@Test
 	public void test06_RemoveNode_RemovesEdgeBetweenNodes() {
-		Person Person1 =new Person ("testUser1");
-		Person neighbor1 = new Person ("testEdge");
-		Person neighbor2 = new Person ("testEdge");
-		
+		Person Person1 = new Person("testUser1");
+		Person neighbor1 = new Person("testEdge");
+		Person neighbor2 = new Person("testEdge");
 
 		graphInstance.addEdge(Person1, neighbor1);
 		graphInstance.addEdge(Person1, neighbor2);
-		
+
 		graphInstance.removeNode(Person1);
-		
+
 		Set<Person> neighbors1 = graphInstance.getNeighbors(neighbor1);
 		Set<Person> neighbors2 = graphInstance.getNeighbors(neighbor2);
 
 		if (neighbors1.contains(Person1)) {
-			fail("Failed to remove edge between users when user was removed.");	
+			fail("Failed to remove edge between users when user was removed.");
 		}
 		if (neighbors2.contains(Person1)) {
-			fail("Failed to remove edge between users when user was removed.");	
+			fail("Failed to remove edge between users when user was removed.");
 		}
 	}
-	
+
+	/**
+	 * Checks that order and size are correct after adding and removing edges
+	 */
 	@Test
 	public void test07_Add05Edges_Remove05Edges_SizeEquals05_OrderEquals00() {
 		Person Person1 = new Person("testUser");
@@ -162,35 +180,44 @@ class GraphTest {
 		graphInstance.addEdge(Person1, Person4);
 		graphInstance.addEdge(Person1, Person5);
 		graphInstance.addEdge(Person1, Person6);
-		
+
 		assertEquals(6, graphInstance.order());
 		assertEquals(5, graphInstance.size());
 
 		graphInstance.removeEdge(Person1, Person2);
 		graphInstance.removeEdge(Person1, Person3);
 		graphInstance.removeEdge(Person1, Person4);
-		
-		assertEquals(6,graphInstance.order());
+
+		assertEquals(6, graphInstance.order());
 		assertEquals(2, graphInstance.size());
 	}
 
+	/**
+	 * Checks that adding node will null parameter doesnt throw exception and
+	 * that it doesnt add a node
+	 */
 	@Test
 	public void test08_AddNullNode_OrderEquals00() {
 		graphInstance.addNode(null);
-		
-		assertEquals(0,graphInstance.order());
+
+		assertEquals(0, graphInstance.order());
 	}
-	
+
+	/**
+	 * Checks that adding edge with null person doesnt add an edge or a new user
+	 */
 	@Test
 	public void test09_AddNullEdge_SizeEquals00() {
 		Person Person = new Person("testUser1");
 		graphInstance.addEdge(Person, null);
-		
-		assertEquals(0,graphInstance.order());
-		assertEquals(0,graphInstance.size());
-	}	
-	
-	//Figure out how to test - but this works correctly
+
+		assertEquals(0, graphInstance.order());
+		assertEquals(0, graphInstance.size());
+	}
+
+	/**
+	 * Checks that user is correctly removed 
+	 */
 	@Test
 	public void test010_AddUser_RemoveUser() {
 		Person Person = new Person("testUser1");
@@ -199,5 +226,9 @@ class GraphTest {
 		graphInstance.addNode(Person);
 		graphInstance.addNode(Person1);
 		graphInstance.removeNode(Person);
-	}	
+		
+		if (graphInstance.getNode("testUser1") != null) {
+			fail("Failed to remove user.");
+		}
+	}
 }
